026b2e7c4fbb8c5167b96ca80826e9fcd5053ef8
Ian S <78048853+Isugg@users.noreply.github.com>
2024-04-19 23:05:08 -0700

    Create main.py
    Create and add main.py
    
    skeleton code to query elastic for certain predefined queries and print the username, host, and event code.

---
+++ main.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from elasticsearch import Elasticsearch
+import pandas as pd
+from queries import queries
+from user_api_key import my_api_key
+
+es = Elasticsearch("https://10.0.0.200:9200", api_key=my_api_key, verify_certs=False)
+
+
+with open('report.csv', 'a+') as report:
+    report.write("Question Number, Username, Hostname, Event code\n")
+    for i,q in enumerate(queries):
+        resp = es.search(index="*security-**", query=q)
+        for hit in resp["hits"]["hits"]:
+            name = hit["_source"]['user']['name']
+            host = hit["_source"]['host']['name']
+            event_code = hit["_source"]['event']['code']
+            report.write(f"Question Number: {i+1},"+name+","+host+","+event_code+"\n")
+
+
+df = pd.read_csv('report.csv').fillna('')
+
+print(df)
